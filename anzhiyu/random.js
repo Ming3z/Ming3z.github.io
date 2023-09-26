39677cae526bc25f0cc807341f6f5e1d794020c4
云服务部_游明焜 <1713442739@qq.com>
2023-09-26 17:51:36 +0800

    Site updated: 2023-09-26 17:51:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/25/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/25/hello-world/","2023/09/26/markdown语法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
